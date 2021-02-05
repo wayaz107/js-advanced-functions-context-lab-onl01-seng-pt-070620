73c5e64b4b7c1b529d98b8dab25d3419177f1f2e
Wardayaz <wayaz107@gmail.com>
2021-02-05 04:42:47 -0600

    Done.

--- index.js
+++ index.js
@@ -59,18 +59,6 @@ let wagesEarnedOnDate = function(dateSought){
     return parseFloat(rawWage.toString())
 }
 
-let allWagesFor = function(){
-    let eligibleDates = this.timeInEvents.map(function(e){
-        return e.date
-    })
-
-    let payable = eligibleDates.reduce(function(memo, d){
-        return memo + wagesEarnedOnDate.call(this, d)
-    }.bind(this), 0)
-
-    return payable
-}
-
 let findEmployeeByFirstName = function(srcArray, firstName) {
   return srcArray.find(function(rec){
     return rec.firstName === firstName
